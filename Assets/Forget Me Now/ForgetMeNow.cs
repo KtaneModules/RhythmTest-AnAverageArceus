c7858387ee666b6fe665ca871e45fe48dbdfd977
AnAverageArceus <65923679+AnAverageArceus@users.noreply.github.com>
2021-02-01 23:57:57 -0700

    Current version of rhythm test
    The most unique module I've created is now available for y'all to use, congratulations

---
+++ Assets/Forget Me Now/ForgetMeNow.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class ForgetMeNow : MonoBehaviour {
+    public KMAudio Audio;
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+
+    public KMSelectable[] Buttons;
+    public Renderer[] Lights;
+    public Material[] LightColors;
+    public GameObject[] Everything;
+    public GameObject TheBigOne;
+    
+    public AudioClip[] Yes;
+    public AudioClip[] No;
+
+    int CorrectAnswer;
+    int FirstFive;
+    int SecondFive;
+    int ThirdFive;
+    int FourthFive;
+    int FifthFive;
+    int OnlyForCalculating;
+
+    string Help;
+    string TheAnswer;
+    int YourNumber = 3;
+
+    bool GoodToGo;
+    bool Started;
+
+    public TextMesh[] ButtonTexts;
+    public TextMesh[] Screens;
+
+    public AudioSource SFX;
+    public AudioClip[] Sounds;
+
+    private static int moduleIdCounter = 1;
+    private int moduleId;
+    
+    private void Awake() {
+        moduleId = moduleIdCounter++;
+        for (int i = 0; i < Buttons.Length; i++) {
+            int j = i;
+            Buttons[i].OnInteract += delegate () {
+                ButtonPress(j);
+                return false;
+            };
+        }
+        Screens[0].text = "";
+        Screens[1].text = "";
+        Screens[2].text = "";
+        CorrectAnswer = UnityEngine.Random.Range(1000, 10000);
+        FirstFive = UnityEngine.Random.Range(10000, 100000);
+        SecondFive = UnityEngine.Random.Range(10000, 20000);
+        ThirdFive = UnityEngine.Random.Range(10000, 100000);
+        FourthFive = UnityEngine.Random.Range(10000, 100000);
+        FifthFive = UnityEngine.Random.Range(10000, 100000);
+        Debug.LogFormat("[More Math #{0}] The starting number here will be {1}.", moduleId, CorrectAnswer);
+        StartCoroutine(TestingPurposes());
+        CalculationOne();
+    }
+
+    private void ButtonPress(int i) {
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, gameObject.transform);
+        Buttons[i].AddInteractionPunch(0.2f);
+        if (GoodToGo == true && YourNumber > 0)
+        {
+            Help += i.ToString();
+            Screens[1].text = Help;
+            YourNumber = YourNumber - 1;
+            if (YourNumber == 0)
+            {
+                StartCoroutine(SolveCheck());
+            }
+        }
+        else if (!Started)
+        {
+            Started = true;
+            StartCoroutine(Startup());
+        }
+    }
+
+    private void CalculationOne()
+    {
+        OnlyForCalculating = FirstFive - CorrectAnswer;
+        Debug.LogFormat("[More Math #{0}] We subtract {2} from {1}. This results in {3}.", moduleId, FirstFive, CorrectAnswer, OnlyForCalculating);
+        CalculationTwo();
+    }
+
+    private void CalculationTwo()
+    {
+        OnlyForCalculating = OnlyForCalculating*SecondFive;
+        Debug.LogFormat("[More Math #{0}] Multiplying the earlier number with {1} gives us {2}.", moduleId, SecondFive, OnlyForCalculating);
+        CalculationThree();
+    }
+
+    private void CalculationThree()
+    {
+        OnlyForCalculating = OnlyForCalculating - 7777;
+        OnlyForCalculating = ThirdFive + OnlyForCalculating;
+        Debug.LogFormat("[More Math #{0}] Subtracting 7777 from that and adding {1} equals {2}.", moduleId, ThirdFive, OnlyForCalculating);
+        CalculationFour();
+    }
+
+    private void CalculationFour()
+    {
+        OnlyForCalculating = FourthFive * 10000 - OnlyForCalculating;
+        OnlyForCalculating = OnlyForCalculating % 1000;
+        if (OnlyForCalculating < 0)
+            OnlyForCalculating = OnlyForCalculating + 1000;
+        Debug.LogFormat("[More Math #{0}] Multiplying {1} by 10000, subtracting the above number from that, modulo 1000, our new number is {2}.", moduleId, FourthFive, OnlyForCalculating);
+        CalculationFive();
+    }
+
+    private void CalculationFive()
+    {
+        OnlyForCalculating = FifthFive % OnlyForCalculating;
+        if (OnlyForCalculating < 100)
+            OnlyForCalculating = OnlyForCalculating + 100;
+        Debug.LogFormat("[More Math #{0}] Finally, taking {1} modulo the above number, the final answer is {2}.", moduleId, FifthFive, OnlyForCalculating);
+    }
+
+    IEnumerator SolveCheck()
+    {
+        Screens[0].text = "";
+        TheAnswer = Help;
+        Debug.LogFormat("[More Math #{0}] Your submission is {1}.", moduleId, TheAnswer);
+        for (int i = 0; i < 50; i++)
+        {
+            FirstFive = UnityEngine.Random.Range(10000, 100000);
+            SecondFive = UnityEngine.Random.Range(10000, 100000);
+            ThirdFive = UnityEngine.Random.Range(10000, 100000);
+            FourthFive = UnityEngine.Random.Range(10000, 100000);
+            FifthFive = UnityEngine.Random.Range(10000, 100000);
+            if (i == 40)
+                SFX.Play();
+            Screens[2].text = FirstFive.ToString() + System.Environment.NewLine + SecondFive.ToString() + System.Environment.NewLine + ThirdFive.ToString() + System.Environment.NewLine + FourthFive.ToString() + System.Environment.NewLine + FifthFive.ToString();
+            yield return new WaitForSeconds(0.02f);
+        }
+        Help = Help.Substring(0, Help.Length - 1);
+        Screens[1].text = Help;
+        for (int i = 0; i < 50; i++)
+        {
+            FirstFive = UnityEngine.Random.Range(10000, 100000);
+            SecondFive = UnityEngine.Random.Range(10000, 100000);
+            ThirdFive = UnityEngine.Random.Range(10000, 100000);
+            FourthFive = UnityEngine.Random.Range(10000, 100000);
+            FifthFive = UnityEngine.Random.Range(10000, 100000);
+            if (i == 40)
+                SFX.Play();
+            Screens[2].text = FirstFive.ToString() + System.Environment.NewLine + SecondFive.ToString() + System.Environment.NewLine + ThirdFive.ToString() + System.Environment.NewLine + FourthFive.ToString() + System.Environment.NewLine + FifthFive.ToString();
+            yield return new WaitForSeconds(0.02f);
+        }
+        Help = Help.Substring(0, Help.Length - 1);
+        Screens[1].text = Help;
+        for (int i = 0; i < 50; i++)
+        {
+            FirstFive = UnityEngine.Random.Range(10000, 100000);
+            SecondFive = UnityEngine.Random.Range(10000, 100000);
+            ThirdFive = UnityEngine.Random.Range(10000, 100000);
+            FourthFive = UnityEngine.Random.Range(10000, 100000);
+            FifthFive = UnityEngine.Random.Range(10000, 100000);
+            if (i == 40)
+                SFX.Play();
+            Screens[2].text = FirstFive.ToString() + System.Environment.NewLine + SecondFive.ToString() + System.Environment.NewLine + ThirdFive.ToString() + System.Environment.NewLine + FourthFive.ToString() + System.Environment.NewLine + FifthFive.ToString();
+            yield return new WaitForSeconds(0.02f);
+        }
+        Help = Help.Substring(0, Help.Length - 1);
+        Screens[1].text = Help;
+        for (int i = 0; i < 50; i++)
+        {
+            FirstFive = UnityEngine.Random.Range(10000, 100000);
+            SecondFive = UnityEngine.Random.Range(10000, 100000);
+            ThirdFive = UnityEngine.Random.Range(10000, 100000);
+            FourthFive = UnityEngine.Random.Range(10000, 100000);
+            FifthFive = UnityEngine.Random.Range(10000, 100000);
+            if (i == 40)
+                SFX.Play();
+            Screens[2].text = FirstFive.ToString() + System.Environment.NewLine + SecondFive.ToString() + System.Environment.NewLine + ThirdFive.ToString() + System.Environment.NewLine + FourthFive.ToString() + System.Environment.NewLine + FifthFive.ToString();
+            yield return new WaitForSeconds(0.02f);
+        }
+        if (OnlyForCalculating.ToString() == TheAnswer)
+        {
+            Debug.LogFormat("[More Math #{0}] Correct! Module solved! Celebration time!", moduleId);
+            Screens[1].text = "YES";
+            Screens[0].text = "";
+            Screens[2].text = "";
+            GoodToGo = false;
+            Module.HandlePass();
+            StartCoroutine(Yep());
+            yield return new WaitForSeconds(2f);
+            Screens[1].text = "YE";
+            yield return new WaitForSeconds(0.65f);
+            Screens[1].text = "Y";
+            yield return new WaitForSeconds(0.65f);
+            Screens[1].text = "";
+        }
+        else
+        {
+            Debug.LogFormat("[More Math #{0}] ...Which, unfortunately, is wrong. Sorry, but I gotta strike ya and reset this thing.", moduleId);
+            Module.HandleStrike();
+            Screens[1].text = "NO";
+            Screens[2].text = "WRONG";
+            SecondFive = UnityEngine.Random.Range(10000, 20000);
+            CorrectAnswer = UnityEngine.Random.Range(1000, 10000);
+            CalculationOne();
+            StartCoroutine(Nope());
+        }
+    }
+
+    IEnumerator TestingPurposes()
+    {
+        for (int j = 0; j < Buttons.Length; j++)
+        {
+            Buttons[j].GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(0.1f);
+        }
+        for (int j = 0; j < Buttons.Length; j++)
+        {
+            Buttons[j].GetComponent<Renderer>().enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        StartCoroutine(TestingPurposes());
+    }
+
+    IEnumerator Startup()
+    {
+        if (!GoodToGo)
+        {
+            Debug.LogFormat("[More Math #{0}: Alright... let's do this.", moduleId);
+            SFX.clip = Sounds[0];
+            SFX.Play();
+            yield return new WaitForSeconds(0.3f);
+            Screens[0].text = "WELC";
+            Screens[1].text = "OME";
+            yield return new WaitForSeconds(2.25f);
+            Screens[2].text = Starting[UnityEngine.Random.Range(0, Starting.Length)];
+            yield return new WaitForSeconds(1.65f);
+            Screens[0].text = "";
+            Screens[1].text = "";
+            Screens[2].text = "";
+            yield return new WaitForSeconds(0.4f);
+        }
+        else
+            GoodToGo = false;
+        SFX.clip = Sounds[1];
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[1].text = "NO";
+        Screens[0].text = CorrectAnswer.ToString();
+        yield return new WaitForSeconds(2f);
+        Screens[2].characterSize = 10f;
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[2].text = FirstFive.ToString();
+        yield return new WaitForSeconds(2f);
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[2].text += System.Environment.NewLine + SecondFive.ToString();
+        yield return new WaitForSeconds(2f);
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[2].text += System.Environment.NewLine + ThirdFive.ToString();
+        yield return new WaitForSeconds(2f);
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[2].text += System.Environment.NewLine + FourthFive.ToString();
+        yield return new WaitForSeconds(2f);
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[2].text += System.Environment.NewLine + FifthFive.ToString();
+        yield return new WaitForSeconds(2f);
+        SFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        Screens[1].text = "";
+        YourNumber = 3;
+        GoodToGo = true;
+    }
+
+    IEnumerator Nope()
+    {
+        yield return new WaitForSeconds(0.01f);
+        SFX.clip = No[UnityEngine.Random.Range(0, No.Length)];
+        SFX.Play();
+        while (SFX.isPlaying)
+        {
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(Startup());
+    }
+
+    IEnumerator Yep()
+    {
+        yield return new WaitForSeconds(0.01f);
+        for (int i = 0; i < ButtonTexts.Length; i++)
+            ButtonTexts[i].text = "";
+        SFX.clip = Yes[0];
+        SFX.Play();
+        for (int j = 0; j < Everything.Length; j++)
+        {
+            Everything[j].GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(0.025f);
+        }
+        yield return new WaitForSeconds(7.1f);
+        var trans = 1f;
+        var col = TheBigOne.GetComponent<Renderer>().material.color;
+        for (int i = 0; i < 100; i++)
+        {
+            trans = trans - 0.01f;
+            col.a = trans;
+            var newColor = new Color(col.r, col.g, col.b, trans);
+            TheBigOne.GetComponent<Renderer>().material.color = newColor;
+            yield return new WaitForSeconds(0.015f);
+        }
+    }
+
+    private static readonly string[] Starting =
+    {
+        "\"[insert\ninspiratio-\n-nal quote\nhere]\"",
+        "\"HELP I'M\nTRAPPED IN\nA MODULE\"",
+        "\"Bet you\ncan't guess\nwhat this\nsound is\nfrom.\"",
+        "\"Uhhhhhhhhhh\nhhhhhhhhhhh\nhhhhhhhhhhh\nhhhhhhhhhhh\nhhhhhhhhhhh\nhhhhhhhhhhh\"",
+        "\"Talk about\na low\nbudget\nflight!\"",
+        "\"I knew\nI should\nhave\ninvited my\ncousin,\nFalco\nLombardi!\"",
+        "\"I can't\nthink of\nany more\nnumbers...\"",
+        "\"Isn't this\nsomething\nhexOS\nalready\ndoes? You\nknow, the\nshuffling\ntext thing?\"",
+        "\"How do any\nof these\nquotes make\na single\nounce of\nsense? Like\nseriously,\nHOW?!\"",
+        "\"No, that's\nwrong!\"",
+        "\"A body\nhas been\ndiscovered!\"",
+        "\"This is so\nsad, Alexa\nplay\nDespacito\"",
+        "\"I spent\nabout an\nhour just\non writing\nthe random\nquotes.\""
+    };
+}
---
+++ Assets/RhythmTest.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using System.Text.RegularExpressions;
+
+public class RhythmTest : MonoBehaviour {
+
+    public KMAudio Audio;
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+
+    public KMSelectable Button;
+    public TextMesh Text;
+    public AudioSource Sounds;
+    public AudioClip[] WhatPlays;
+    public Material[] LightColors;
+    public Renderer[] Test1;
+    public Renderer[] Test2;
+    public Renderer[] Test3;
+    public Renderer[] Test4;
+    public Renderer[] Test5;
+    public Renderer[] Test6;
+    public Renderer[] Test7;
+    public Renderer[] Test8;
+    public Renderer[] Test9;
+    public Renderer[] Test10;
+    public Renderer[] Test11;
+    public Renderer[] Test12;
+    public Renderer[] Test13;
+    public Renderer[] Test14;
+    public Renderer[] Test15;
+    public Renderer[] Test16;
+    public Renderer TheLight;
+
+    bool NotReady = true;
+    int Beat = 8;
+    int Resetting;
+    int CurrentTest = 1;
+    bool Hittable;
+    bool Press;
+    bool TwitchPlaysCompatibility;
+    int TPBonus;
+    bool Prepped;
+    bool Playing;
+    bool LatencyComp;
+    float Latency;
+    bool Paused;
+    bool InvalidPress;
+
+    private static int moduleIdCounter = 1;
+    private int moduleId;
+    private bool ModuleSolved;
+
+    void Awake()
+    {
+        moduleId = moduleIdCounter++;
+        Button.OnInteract += delegate () { Pressed(); return false; };
+        Text.characterSize = 0.3f;
+        Text.text = "Rhythm Test";
+    }
+    void Pressed()
+    {
+        Sounds.PlayOneShot(WhatPlays[2]);
+        Button.AddInteractionPunch(0.2f);
+        if (!ModuleSolved)
+        {
+            if (LatencyComp)
+                LatencyComp = false;
+            else if (NotReady)
+            {
+                Text.characterSize = 0.7f;
+                StartCoroutine(ItsTestTime());
+            }
+            else if (Hittable)
+            {
+                Press = true;
+                Hittable = false;
+            }
+            else if (!TwitchPlaysCompatibility)
+            {
+                Module.HandleStrike();
+                InvalidPress = true;
+            }
+        }
+    }
+
+    void Start()
+    {
+        Debug.LogFormat("[Rhythm Test #{0}] Welcome to the rhythm test! Let's see how good your rhythm really is. Good luck!", moduleId);
+    }
+
+    IEnumerator TwitchPlaysImportance()
+    {
+        Text.characterSize = 1f;
+        for (int i = 10; i > 0; i = i - 1)
+        {
+            Text.text = i.ToString();
+            Sounds.PlayOneShot(WhatPlays[0]);
+            yield return new WaitForSeconds(1f);
+        }
+        Text.text = "0";
+        Sounds.PlayOneShot(WhatPlays[1]);
+        LatencyComp = true;
+        Prepped = true;
+        while (LatencyComp)
+        {
+            yield return new WaitForSeconds(0.01f);
+            Latency += 0.02f;
+        }
+        Text.characterSize = 0.7f;
+        Text.text = Latency.ToString();
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(ItsTestTime());
+    }
+
+    IEnumerator ItsTestTime()
+    {
+        NotReady = false;
+        Text.text = "READY";
+        yield return new WaitForSeconds(3f);
+        Text.characterSize = 1f;
+        Text.text = "";
+        yield return new WaitForSeconds(2f);
+        for (int i = 1; i < 17; i++)
+        {
+            InvalidPress = false;
+            Playing = true;
+            i = CurrentTest;
+            Hittable = false;
+            for (int j = 0; j < 7; j++)
+            {
+                Beat = Beat - 1;
+                    yield return new WaitForSeconds(0.5f);
+                    switch (i)
+                    {
+                        case 1: Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); break;
+                        case 2: Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); break;
+                        case 3: Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); break;
+                        case 4: if (Beat >= 2) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 5: if (Beat >= 2) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 6: if (Beat >= 3) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 7: if (Beat >= 3) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 8: if (Beat >= 4) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 9: if (Beat >= 4) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 10: if (Beat >= 5) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 11: if (Beat >= 5) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 12: if (Beat >= 6) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 13: if (Beat >= 6) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 14: if (Beat >= 7) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 15: if (Beat >= 7) { Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); } else Text.text = ""; break;
+                        case 16: Text.text = Beat.ToString(); Sounds.PlayOneShot(WhatPlays[0]); break;
+                    }
+                TheLight.material = LightColors[0];
+            }
+            yield return new WaitForSeconds(0.35f);
+            if (TwitchPlaysCompatibility)
+            {
+                yield return new WaitForSeconds(0.15f);
+                Text.text = "0";
+                Sounds.PlayOneShot(WhatPlays[1]);
+                yield return new WaitForSeconds(Latency - 0.15f);
+            }
+            Hittable = true;
+            Press = false;
+            for (int j = 0; j < 9; j++)
+            {
+                if (j == 4)
+                {
+                    Text.text = "0";
+                    Sounds.PlayOneShot(WhatPlays[1]);
+                }
+                if (!Press && Hittable && (i < 5))
+                {
+                    switch (i)
+                    {
+                        case 1: Test1[j].material = LightColors[1]; if (j == 5) Test1[j - 1].material = LightColors[2]; else if (j != 0) Test1[j - 1].material = LightColors[0]; yield return new WaitForSeconds(0.025f); break;
+                        case 2: Test2[j].material = LightColors[1]; if (j == 5) Test2[j - 1].material = LightColors[2]; else if (j != 0) Test2[j - 1].material = LightColors[0]; yield return new WaitForSeconds(0.025f); break;
+                        case 3: Test3[j].material = LightColors[1]; if (j == 5) Test3[j - 1].material = LightColors[2]; else if (j != 0) Test3[j - 1].material = LightColors[0]; yield return new WaitForSeconds(0.025f); break;
+                        case 4: Test4[j].material = LightColors[1]; if (j == 5) Test4[j - 1].material = LightColors[2]; else if (j != 0) Test4[j - 1].material = LightColors[0]; yield return new WaitForSeconds(0.025f); break;
+                    }
+                }
+                else if (!Press && Hittable && (i < 5) && (j == 8))
+                {
+                    switch (i)
+                    {
+                        case 1: Test1[8].material = LightColors[0]; break;
+                        case 2: Test2[8].material = LightColors[0]; break;
+                        case 3: Test3[8].material = LightColors[0]; break;
+                        case 4: Test4[8].material = LightColors[0]; break;
+                    }
+                }
+                else if (Press && (i > 4))
+                {
+                    switch (i)
+                    {
+                        case 5: Test5[j].material = LightColors[1]; break;
+                        case 6: Test6[j].material = LightColors[1]; break;
+                        case 7: Test7[j].material = LightColors[1]; break;
+                        case 8: Test8[j].material = LightColors[1]; break;
+                        case 9: Test9[j].material = LightColors[1]; break;
+                        case 10: Test10[j].material = LightColors[1]; break;
+                        case 11: Test11[j].material = LightColors[1]; break;
+                        case 12: Test12[j].material = LightColors[1]; break;
+                        case 13: Test13[j].material = LightColors[1]; break;
+                        case 14: Test14[j].material = LightColors[1]; break;
+                        case 15: Test15[j].material = LightColors[1]; break;
+                        case 16: Test16[j].material = LightColors[1]; break;
+                    }
+                    if (j < 4)
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You pressed a bit early...", moduleId, i);
+                    else if (j == 4)
+                    {
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You hit the beat perfectly!", moduleId, i);
+                        TPBonus++;
+                    }
+                    else if (j > 4)
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You were a bit late...", moduleId, i);
+                    Press = false;
+                }
+                else if (Press && (i < 5))
+                {
+                    if (j < 5)
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You pressed a bit early...", moduleId, i);
+                    else if (j == 5)
+                    {
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You hit the beat perfectly!", moduleId, i);
+                        TPBonus++;
+                    }
+                    else if (j > 5)
+                        Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You were a bit late...", moduleId, i);
+                    Press = false;
+                    yield return new WaitForSeconds(0.025f);
+                }
+                else if (i > 4)
+                    yield return new WaitForSeconds(0.025f);
+            }
+            if (Hittable && !Press)
+            {
+                if (!TwitchPlaysCompatibility && !InvalidPress)
+                {
+                    Module.HandleStrike();
+                    TheLight.material = LightColors[1];
+                    Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You didn't even press the button. Sorry, but that's a strike.", moduleId, i);
+                }
+                else
+                    Debug.LogFormat("[Rhythm Test #{0}] Test {1}: You didn't press the button near the right time.", moduleId, i);
+            }
+            CurrentTest = CurrentTest + 1;
+            Beat = 8;
+        }
+            Text.text = "";
+            yield return new WaitForSeconds(3f);
+            Module.HandlePass();
+            ModuleSolved = true;
+            TheLight.material = LightColors[2];
+            Debug.LogFormat("[Rhythm Test #{0}] Test complete! Congratulations.", moduleId);
+            Text.characterSize = 0.7f;
+            Text.text = "Done!";
+    }
+    /*/
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} prep (prepares the module for TP usage, REQUIRED TO START) | !{0} press (presses the button, the timing will be relative to your latency which is judged by '!{0} prep', will also reject command if unprepped) | !{0} pause (pauses/restarts current test, use only if lag spike occurs)";
+#pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        TwitchPlaysCompatibility = true;
+        string[] HellYeah = command.Split(' ');
+        if (Regex.IsMatch(HellYeah[0], @"^\s*prep\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (HellYeah.Length != 1)
+                yield return "sendtochaterror Too many arguments!";
+            else
+            {
+                StartCoroutine(TwitchPlaysImportance());
+            }
+        }
+        else if (Regex.IsMatch(HellYeah[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (HellYeah.Length != 1)
+                yield return "sendtochaterror Too many arguments!";
+            else if (!Prepped)
+                yield return "sendtochaterror Please prep the module first!";
+            else
+                Button.OnInteract();
+        }
+        else if (Regex.IsMatch(HellYeah[0], @"^\s*pause\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (HellYeah.Length != 1)
+                yield return "sendtochaterror Too many arguments!";
+            else if (!Playing)
+                yield return "sendtochaterror Please wait until playing!";
+            else if (!Paused)
+                Paused = true;
+            else
+            {
+                Paused = false;
+                LatencyComp = true;
+                StartCoroutine(TwitchPlaysImportance());
+            }
+        }
+        else
+        {
+            yield return null;
+            yield return "sendtochaterror Invalid command. Please try again.";
+        }
+    }
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+        Module.HandlePass();
+        Debug.LogFormat("[Rhythm Test #{0}] Autosolve command received.", moduleId);
+    }
+    /*/
+}
